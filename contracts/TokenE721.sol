32e22ca3157fc9b671f6436aaea6ddaf46999258
Ebenezer-ebu <eifezulike@gmail.com>
2022-03-18 13:30:33 +0100

    update contracts

--- contracts/TokenE721.sol
+++ contracts/TokenE721.sol
@@ -10,6 +10,7 @@ contract MyTokenNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    mapping(string => uint8) hashes;
 
     constructor() ERC721("MyTokenNFT", "MTN") {}
 
@@ -33,5 +34,15 @@ contract MyTokenNFT is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function awardItem(address recipient, string memory hash, string memory metadata) public returns (uint256) {
+        require(hashes[hash] != 1);
+        hashes[hash] = 1;
+        _tokenIdCounter.increment();
+        uint256 newItemId = _tokenIdCounter.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, metadata);
+        return newItemId;
     }
 }
